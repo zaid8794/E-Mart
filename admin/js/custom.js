f0a24cbdfe0aaad787a5747ea4cfbb40e45e9087
Zaid Vora <zaidvora9@gmail.com>
2024-04-26 01:44:08 +0530

    active menu on contactus done

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -2,6 +2,7 @@ var home = document.getElementById("index");
 var category = document.getElementById("category");
 var brand = document.getElementById("brand");
 var product = document.getElementById("product");
+var contactus = document.getElementById("contactus");
 var url = window.location.pathname;
 var filename = url.substring(url.lastIndexOf("/") + 1);
 if (filename == "index.php") {
@@ -12,4 +13,6 @@ if (filename == "index.php") {
   brand.classList.add("active");
 } else if (filename == "product.php") {
   product.classList.add("active");
+} else if (filename == "contactus.php") {
+  contactus.classList.add("active");
 }
